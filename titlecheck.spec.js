c9a99d13122f404c8fe0db1c5ecdcf0fbe12ace2
Balaji <mgbalaji89@gmail.com>
2026-08-10 17:58:58 +0530

    Update titlecheck.spec.js

--- titlecheck.spec.js
+++ titlecheck.spec.js
@@ -4,6 +4,7 @@ const baseURL = 'https://testautomationpractice.blogspot.com/';
 
 test('Check the title of the page ', async( { page} ) => {
     await page.goto(baseURL);
+    //Purposely give the wrong actual title to check the test case failiure
     const actualTitle = 'Automation Testing Practiceee';
     await expect(page).toHaveTitle(actualTitle);
 });
