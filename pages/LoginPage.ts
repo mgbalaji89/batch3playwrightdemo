675abde2d18ea1c0294cc25c3309b9d0add8ed58
Balaji <mgbalaji89@gmail.com>
2026-09-14 18:52:28 +0530

    Update LoginPage.ts

--- pages/LoginPage.ts
+++ pages/LoginPage.ts
@@ -1,24 +1,26 @@
-import {test, expect } from '@playwright/test';
-import { LoginPage } from '../pages/LoginPage';
-
-import * as credential from '../test-data/users.json';
-import { DashboardPage } from '../pages/DashboardPage';
-
-test('User can signin with Valid Credentials ', async( { page }) => {
-    //This is completly page object model
-    //Creating the new instance for the Login Page , Navigating to Login URL and passing the valid username and password 
-    // Then Clicking the Submit Button 
-    const loginPage = new LoginPage(page); 
-    const dashboardPage = new DashboardPage(page);
-    //Getting the page title after successful login and storing it into pageTitle variable
-    await loginPage.goto();
-    //await loginPage.signIn('student','Password123',);
-    await loginPage.signIn(credential.username,credential.password);
-    const pageTitle = await loginPage.getPageTitle();
-    //Logged In Successfully | Practice Test Automation
-    //Performing the assertion of the page title after successful Login
-    expect(pageTitle).toBe('Logged In Successfully | Practice Test Automation');
-    console.log(pageTitle);
-    await dashboardPage.verifyLogoutButton();
-    await dashboardPage.verifySuccessMessage();
-});
+import {type Locator, type Page} from '@playwright/test';
+export class LoginPage{
+    readonly page: Page;
+    readonly username: Locator;
+    readonly passwordInput: Locator;
+    readonly SignInButton: Locator;
+    readonly errorMessage: Locator;
+    constructor(page: Page) {
+        this.page = page;
+        this.username = page.getByLabel('Username');
+	    this.passwordInput = page.getByLabel('Password');
+	    this.SignInButton = page.getByRole('button', { name: 'Submit'});
+	    this.errorMessage = page.getByTestId('login-error');
+    }
+    async goto() {
+        await this.page.goto('/practice-test-login');
+    }
+    async signIn(user: string, password: string){
+        await this.username.fill(user);
+        await this.passwordInput.fill(password);
+        await this.SignInButton.click();
+    }
+    async getPageTitle() {
+        return this.page.title();
+    }
+}
